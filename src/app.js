257bc544e1161e6603cf4511596cff89e1b94f47
Jordan Acevedo <jordanace83@gmail.com>
2021-11-18 12:48:20 -0600

    Building Environment

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>MY APP</h1>
+      <h2>TESTING</h2>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
