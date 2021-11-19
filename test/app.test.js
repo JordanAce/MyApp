ae1967dc4a529aa5d2740a4b662c281b8c7f3882
Jordan Acevedo <jordanace83@gmail.com>
2021-11-19 15:36:51 -0600

    Added Jest Test Library

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+import { exportAllDeclaration } from '@babel/types'
+import React from 'react'
+import renderer from 'react-test-renderer'
+// import App from '../src/app.js'
+
+describe('It should test my app', () => {
+  test('One Equals One', () => {
+    expect(1).toEqual(1);
+  })
+})
